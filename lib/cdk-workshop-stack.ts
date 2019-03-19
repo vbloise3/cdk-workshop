c14d6a06cbc2fd3f7c3df264d7834f9c50404525
Vince Bloise <vbloise3@gmail.com>
2019-03-19 17:41:39 -0400

    Added lambda function and api gateway

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,17 +1,21 @@
-import sns = require('@aws-cdk/aws-sns');
-import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/cdk');
+import lambda = require('@aws-cdk/aws-lambda');
+import apigw = require('@aws-cdk/aws-apigateway');
 
 export class CdkWorkshopStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeoutSec: 300
-    });
+        // defines an AWS Lambda resource
+        const hello = new lambda.Function(this, 'HelloHandler', {
+            runtime: lambda.Runtime.NodeJS810,      // execution environment
+            code: lambda.Code.asset('lambda'),  // code loaded from the "lambda" directory
+            handler: 'hello.handler'                // file is "hello", function is "handler"
+        });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.subscribeQueue(queue);
-  }
+        // defines an API Gateway REST API resource backed by our "hello" function.
+        new apigw.LambdaRestApi(this, 'Endpoint', {
+            handler: hello
+        });
+    }
 }
