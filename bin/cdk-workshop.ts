29cad02307e003c5c80c6c39871f47e2d9ccde0e
Vince Bloise <vbloise3@gmail.com>
2019-03-19 16:54:46 -0400

    Initial commit

---
+++ bin/cdk-workshop.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import cdk = require('@aws-cdk/cdk');
+import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
+
+const app = new cdk.App();
+new CdkWorkshopStack(app, 'CdkWorkshopStack');
+app.run();
